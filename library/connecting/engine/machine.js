88941cc0c382c5ba8c9378170582abb7ca54f627
rulllzfuqi <syahrulafwanf1o@gmail.com>
2025-08-17 22:18:33 +0800

    Create machine.js

---
+++ library/connecting/engine/machine.js
@@ -0,0 +1,30 @@
+import makeWASocket, { makeInMemoryStore, jidDecode } from '@whiskeysockets/baileys'
+import Pino from 'pino'
+import fetch from 'node-fetch'
+
+export const fetchJson = async (url, options) => {
+  const res = await fetch(url, options)
+  return await res.json()
+}
+
+export const getBuffer = async (url, options) => {
+  const res = await fetch(url, options)
+  const buffer = await res.arrayBuffer()
+  return Buffer.from(buffer)
+}
+
+export const store = makeInMemoryStore({
+  logger: Pino({ level: 'fatal' }).child({ level: 'fatal' })
+})
+
+export const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+export const decodeJid = jid => {
+  if (!jid) return jid
+  if (/:\d+@/gi.test(jid)) {
+    const decode = jidDecode(jid) || {}
+    return decode.user && decode.server ? decode.user + '@' + decode.server : jid
+  } else {
+    return jid
+  }
+}
